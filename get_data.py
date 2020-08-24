f41de235ed109354c2f675fc5f4f29f526a89205
ViktorRaboshchuk <30927030+ViktorRaboshchuk@users.noreply.github.com>
2020-08-24 18:26:52 +0300

    Update get_data.py

--- get_data.py
+++ get_data.py
@@ -3,6 +3,7 @@ import random
 import sqlite3
 import sys
 from selenium import webdriver
+import os
 from selenium.webdriver.common.keys import Keys
 from bs4 import BeautifulSoup as bs
 import schedule
@@ -21,7 +22,12 @@ bitly_token = ['87560fe9b0f92d08b566103bf44591589c4e33c8']
 
 def tag_search(tags):
     # Logging
-    browser = webdriver.Chrome('/chromedriver.exe')
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--no-sandbox")
+    browser = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
     browser.get("https://www.linkedin.com")
     browser.maximize_window()
     browser.implicitly_wait(3)
